7769739c5aaf481bfedd289facd055e918ab0707
Alberto Moral <alberto.moral.g@gmail.com>
2017-08-13 10:17:24 +0200

    Create dispose bag property in example

--- Example/SlackWebAPIKit/ViewController.swift
+++ Example/SlackWebAPIKit/ViewController.swift
@@ -1,21 +1,23 @@
 import UIKit
+import RxSwift
 import SlackWebAPIKit
 
 class ViewController: UIViewController {
 
     let postUserMessageUseCase = FindUserAndPostMessageUseCase()
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-     
-        _ = postUserMessageUseCase.execute(text: "Test", user: "alberto")
+        
+        postUserMessageUseCase.execute(text: "Test", user: "alberto")
             .subscribe(onNext: { (isSent) in
                 print("onNext \(isSent)")
             }, onError: { (error) in
                 print("onError \(error)")
             }, onCompleted: {
                 print("onComplete")
-            })
+        }).addDisposableTo(disposeBag)
     }
 }
 
